732ebc70759b3814a7f1090b518dcb17f7acc572
molo06002 <molo06002@E-PW0234KQ>
2023-09-15 10:15:39 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; 
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.Clear();
+            Console.WriteLine("Hur långt hoppade Alma?");
+            string m = Console.ReadLine();
+            int b = Convert.ToInt32(m);
+            int O = y - b;
+            Console.WriteLine(O);
+        }
+    }
+}
